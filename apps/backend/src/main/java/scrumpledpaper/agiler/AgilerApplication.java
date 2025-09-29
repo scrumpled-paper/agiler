ccef68dea55e24a8e33319a94efcf3e5541ed994
seungsje <seungsje@student.42seoul.kr>
2025-09-29 16:50:29 +0900

    feat(baseentity): add baseentity

--- apps/backend/src/main/java/scrumpledpaper/agiler/AgilerApplication.java
+++ apps/backend/src/main/java/scrumpledpaper/agiler/AgilerApplication.java
@@ -2,8 +2,10 @@ package scrumpledpaper.agiler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AgilerApplication {
 
 	public static void main(String[] args) {
