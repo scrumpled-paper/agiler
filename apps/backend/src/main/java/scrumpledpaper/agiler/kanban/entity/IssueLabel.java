bf3d9dc489355a573301c1ab57f37ecff362737e
seungsje <seungsje@student.42seoul.kr>
2025-09-29 16:55:33 +0900

    feat(kanban): add kanban domain entity classes

---
+++ apps/backend/src/main/java/scrumpledpaper/agiler/kanban/entity/IssueLabel.java
@@ -0,0 +1,32 @@
+package scrumpledpaper.agiler.kanban.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import scrumpledpaper.agiler.common.BaseEntity;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "issue_label")
+public class IssueLabel extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "issue_id")
+	private Issue issue;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "label_id")
+	private Label label;
+}
