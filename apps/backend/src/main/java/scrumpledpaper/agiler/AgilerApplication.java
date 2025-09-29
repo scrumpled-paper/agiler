ce95544f928f854c2296dbda063c9d63416edaac
HeeSung Choi <gladhee@icloud.com>
2025-09-29 15:34:09 +0900

    feat(backend): add initial project structure with Gradle, Spring Boot, and test configuration

---
+++ apps/backend/src/main/java/scrumpledpaper/agiler/AgilerApplication.java
@@ -0,0 +1,13 @@
+package scrumpledpaper.agiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgilerApplication.class, args);
+	}
+
+}
---
+++ apps/backend/src/test/java/scrumpledpaper/agiler/TestAgilerApplication.java
@@ -0,0 +1,11 @@
+package scrumpledpaper.agiler;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestAgilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(AgilerApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
