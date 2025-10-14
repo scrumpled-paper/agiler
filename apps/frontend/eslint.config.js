20604ee832cb2fc03290edd5483cfc0ac5641f22
jiminChoi <jimchoi@student.42seoul.co.kr>
2025-10-15 02:39:26 +0900

    chore(config): update eslint config to legacy format
    - change from eslint flat config to legacy format
    - migrate from esm to commonjs for module compatibility
    -

--- apps/frontend/eslint.config.js
+++ apps/frontend/eslint.config.js
@@ -1,4 +1,3 @@
-// @ts-nocheck
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
@@ -6,18 +5,21 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import prettier from 'eslint-plugin-prettier'
 import prettierConfig from 'eslint-config-prettier'
-import tsParser from '@typescript-eslint/parser'
 
 export default tseslint.config(
   {
     ignores: ['dist', 'node_modules', 'build'],
   },
+
   {
-    files: ['**/*.{ts,tsx,js,jsx}'],
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-      parser: tsParser, // TypeScript 파서 추가
       parserOptions: {
         ecmaFeatures: {
           jsx: true,
@@ -25,29 +27,17 @@ export default tseslint.config(
       },
     },
     plugins: {
-      '@typescript-eslint': tseslint.plugin, // 추가
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      prettier: prettier,
+      'prettier': prettier,
     },
     rules: {
-      ...js.configs.recommended.rules,
-      ...tseslint.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       ...prettierConfig.rules,
-
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       'prettier/prettier': 'error',
-
-      'no-console': ['warn', { allow: ['warn', 'error'] }],
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        { argsIgnorePattern: '^_' },
-      ],
+      'no-console': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     },
   }
 )
