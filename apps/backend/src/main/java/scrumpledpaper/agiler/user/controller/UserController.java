bf7de3cd5afbf868b66e336a1fc4afd02389990f
seungsje <seungsje@student.42seoul.kr>
2025-10-15 11:35:57 +0900

    refactor(authcontroller): separate user controller from auth controller

--- apps/backend/src/main/java/scrumpledpaper/agiler/user/controller/UserController.java
+++ apps/backend/src/main/java/scrumpledpaper/agiler/user/controller/UserController.java
@@ -2,7 +2,6 @@ package scrumpledpaper.agiler.user.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import scrumpledpaper.agiler.common.resolver.Login;
-import scrumpledpaper.agiler.user.dto.TokenResponseDto;
 import scrumpledpaper.agiler.user.dto.UserDto;
 import scrumpledpaper.agiler.user.dto.UserResDto;
 import scrumpledpaper.agiler.user.service.UserService;
@@ -20,13 +18,6 @@ import scrumpledpaper.agiler.user.service.UserService;
 @RequiredArgsConstructor
 public class UserController {
 	private final UserService userService;
-
-	@PostMapping("/login") // todo
-	public ResponseEntity<TokenResponseDto> login(@RequestBody String email) {
-		TokenResponseDto tokenResponseDto = userService.login(email);
-		return ResponseEntity.created(null).body(tokenResponseDto);
-	}
-
 
 	@GetMapping("/users")
 	public ResponseEntity<UserResDto> getUser(@Parameter(hidden = true) @Login UserDto userDto) {
