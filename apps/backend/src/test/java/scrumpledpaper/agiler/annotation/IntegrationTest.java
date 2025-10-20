c9e10d56c37a0b8492657b9fffab8ab59f316c0c
seungsje <seungsje@student.42seoul.kr>
2025-10-20 16:04:04 +0900

    test(testcontainer): add integrationtest test container

--- apps/backend/src/test/java/scrumpledpaper/agiler/annotation/IntegrationTest.java
+++ apps/backend/src/test/java/scrumpledpaper/agiler/annotation/IntegrationTest.java
@@ -5,14 +5,20 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import scrumpledpaper.agiler.config.TestcontainersConfiguration;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureMockMvc
+@Import(TestcontainersConfiguration.class)
 @Transactional
 public @interface IntegrationTest {
 }
