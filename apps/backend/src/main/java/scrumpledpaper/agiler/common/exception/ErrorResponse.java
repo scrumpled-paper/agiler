83260e78e601fda11ca9192503c479a8797998dc
seungsje <seungsje@student.42seoul.kr>
2025-10-20 14:23:12 +0900

    feat(globalhandler): add valid global exception handler and error code

--- apps/backend/src/main/java/scrumpledpaper/agiler/common/exception/ErrorResponse.java
+++ apps/backend/src/main/java/scrumpledpaper/agiler/common/exception/ErrorResponse.java
@@ -1,16 +1,26 @@
 package scrumpledpaper.agiler.common.exception;
 
+import java.util.Map;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 class ErrorResponse {
+	private int status;
 	private final String code;
 	private final String message;
+	private Map<String, String> fieldErrors;
 
 	public ErrorResponse(ErrorCode errorCode) {
+		this.status = errorCode.getStatus();
 		this.code = errorCode.getCode();
 		this.message = errorCode.getMessage();
+	}
+
+	public ErrorResponse(ErrorCode errorCode, Map<String, String> fieldErrors) {
+		this(errorCode);
+		this.fieldErrors = fieldErrors;
 	}
 }
