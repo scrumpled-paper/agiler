c66c4c1bb0fed1f0cb08e71ef471eb947a4c57d3
Heesung Choi <gladhee@icloud.com>
2025-10-22 11:13:34 +0900

    refactor(UserTest): refactor UserTest to use IntegrationTest

--- apps/backend/src/test/java/scrumpledpaper/agiler/user/UserTest.java
+++ apps/backend/src/test/java/scrumpledpaper/agiler/user/UserTest.java
@@ -1,22 +1,19 @@
 package scrumpledpaper.agiler.user;
 
-import static org.assertj.core.api.Assertions.*;
-
-import java.util.Optional;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
+import scrumpledpaper.agiler.annotation.IntegrationTest;
 import scrumpledpaper.agiler.fixture.UserFixture;
 import scrumpledpaper.agiler.user.entity.User;
 import scrumpledpaper.agiler.user.repository.UserRepository;
 
-@SpringBootTest
-@Transactional
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@IntegrationTest
 public class UserTest {
 	@Autowired
 	private UserRepository userRepository;
