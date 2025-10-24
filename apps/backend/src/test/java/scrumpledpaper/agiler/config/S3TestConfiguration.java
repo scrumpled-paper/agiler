63a4a7979335b82b3a1d4d49ec7fc99f0da95a22
Heesung Choi <gladhee@icloud.com>
2025-10-24 17:38:20 +0900

    feat(S3TestConfiguration): add configuration for LocalStack S3 testing

---
+++ apps/backend/src/test/java/scrumpledpaper/agiler/config/S3TestConfiguration.java
@@ -0,0 +1,44 @@
+package scrumpledpaper.agiler.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class S3TestConfiguration {
+
+	@Bean
+	public LocalStackContainer localStackContainer() {
+		return new LocalStackContainer(DockerImageName.parse("localstack/localstack:3"))
+				.withServices(LocalStackContainer.Service.S3);
+	}
+
+	@Bean
+	@Primary
+	public AmazonS3 amazonS3Client(LocalStackContainer localStackContainer) {
+		AwsClientBuilder.EndpointConfiguration endpointConfig = new AwsClientBuilder.EndpointConfiguration(
+				localStackContainer.getEndpointOverride(LocalStackContainer.Service.S3).toString(),
+				localStackContainer.getRegion()
+		);
+
+		AWSCredentials credentials = new BasicAWSCredentials(
+				localStackContainer.getAccessKey(),
+				localStackContainer.getSecretKey()
+		);
+
+		return AmazonS3ClientBuilder.standard()
+				.withEndpointConfiguration(endpointConfig)
+				.withCredentials(new AWSStaticCredentialsProvider(credentials))
+				.withPathStyleAccessEnabled(true)
+				.build();
+	}
+
+}
