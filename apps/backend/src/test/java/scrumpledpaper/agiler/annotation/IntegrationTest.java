3ba571a979deadec289502910a3130d88262c4bf
Heesung Choi <gladhee@icloud.com>
2025-10-24 17:38:10 +0900

    refactor(IntegrationTest): remove transaction annotation

--- apps/backend/src/test/java/scrumpledpaper/agiler/annotation/IntegrationTest.java
+++ apps/backend/src/test/java/scrumpledpaper/agiler/annotation/IntegrationTest.java
@@ -1,18 +1,16 @@
 package scrumpledpaper.agiler.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
-
 import scrumpledpaper.agiler.config.TestcontainersConfiguration;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
@@ -21,6 +19,5 @@ import scrumpledpaper.agiler.config.TestcontainersConfiguration;
 @AutoConfigureMockMvc
 @Import(TestcontainersConfiguration.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Transactional
 public @interface IntegrationTest {
 }
