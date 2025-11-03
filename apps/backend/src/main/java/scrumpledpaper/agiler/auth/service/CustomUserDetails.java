5962c36e31d89235f152ccdd405dc99ce7dcf8c1
Heesung Choi <gladhee@icloud.com>
2025-11-03 15:20:55 +0900

    refactor(CustomUserDetails): delegate account status checks to UserDetails interface

--- apps/backend/src/main/java/scrumpledpaper/agiler/auth/service/CustomUserDetails.java
+++ apps/backend/src/main/java/scrumpledpaper/agiler/auth/service/CustomUserDetails.java
@@ -39,22 +39,22 @@ public class CustomUserDetails implements UserDetails {
 
 	@Override
 	public boolean isAccountNonExpired() {
-		return true;
+		return UserDetails.super.isAccountNonExpired();
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
-		return true;
+		return UserDetails.super.isAccountNonLocked();
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
-		return true;
+		return UserDetails.super.isCredentialsNonExpired();
 	}
 
 	@Override
 	public boolean isEnabled() {
-		return true;
+		return UserDetails.super.isEnabled();
 	}
 
 }
