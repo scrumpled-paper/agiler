c6f74e3b5a59d48f4ecc58af2c9442653832b723
Heesung Choi <gladhee@icloud.com>
2025-11-05 12:15:35 +0900

    refactor(OAuth2UserInfo): remove getter

--- apps/backend/src/main/java/scrumpledpaper/agiler/auth/oauth2/userinfo/OAuth2UserInfo.java
+++ apps/backend/src/main/java/scrumpledpaper/agiler/auth/oauth2/userinfo/OAuth2UserInfo.java
@@ -1,22 +1,20 @@
 package scrumpledpaper.agiler.auth.oauth2.userinfo;
 
-import lombok.Getter;
-
 import java.util.Map;
 
-@Getter
 public abstract class OAuth2UserInfo {
-    protected Map<String, Object> attributes;
+	protected Map<String, Object> attributes;
 
-    public OAuth2UserInfo(Map<String, Object> attributes) {
-        this.attributes = attributes;
-    }
+	public OAuth2UserInfo(Map<String, Object> attributes) {
+		this.attributes = attributes;
+	}
 
 	public abstract String getId();
 
-    public abstract String getName();
+	public abstract String getName();
 
-    public abstract String getEmail();
+	public abstract String getEmail();
 
-    public abstract String getImageUrl();
+	public abstract String getImageUrl();
+
 }
