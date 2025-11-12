0185f35e324a27407950e7b1b32aaa3efcd57a79
seungsje <seungsje@student.42seoul.kr>
2025-11-12 17:59:29 +0900

    chore(LabelRepository): remove no use import

--- apps/backend/src/main/java/scrumpledpaper/agiler/kanban/repository/LabelRepository.java
+++ apps/backend/src/main/java/scrumpledpaper/agiler/kanban/repository/LabelRepository.java
@@ -3,8 +3,8 @@ package scrumpledpaper.agiler.kanban.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import scrumpledpaper.agiler.kanban.entity.Label;
-import scrumpledpaper.agiler.project.entity.Project;
 
 public interface LabelRepository extends JpaRepository<Label, Long> {
 	List<Label> findByProjectId(Long projectId);
