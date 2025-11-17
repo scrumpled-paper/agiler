7b30152a37d82e4fa4bf71b0e16b5a21ecebf262
seungsje <seungsje@student.42seoul.kr>
2025-11-17 12:34:53 +0900

    test(ProjectControllerTest): add default scrum template test

--- apps/backend/src/testFixtures/java/scrumpledpaper/agiler/common/TestDataFactory.java
+++ apps/backend/src/testFixtures/java/scrumpledpaper/agiler/common/TestDataFactory.java
@@ -14,7 +14,9 @@ import scrumpledpaper.agiler.project.entity.Role;
 import scrumpledpaper.agiler.project.repository.ProfileRepository;
 import scrumpledpaper.agiler.project.repository.ProjectRepository;
 import scrumpledpaper.agiler.template.entity.IssueTemplate;
+import scrumpledpaper.agiler.template.entity.ScrumTemplate;
 import scrumpledpaper.agiler.template.repository.IssueTemplateRepository;
+import scrumpledpaper.agiler.template.repository.ScrumTemplateRepository;
 import scrumpledpaper.agiler.user.entity.User;
 import scrumpledpaper.agiler.user.repository.UserRepository;
 
@@ -32,6 +34,7 @@ public class TestDataFactory {
 	private final ProfileRepository profileRepository;
 	private final ProjectRepository projectRepository;
 	private final IssueTemplateRepository issueTemplateRepository;
+	private final ScrumTemplateRepository scrumTemplateRepository;
 	private final EntityManager entityManager;
 
 	public Image createDefaultImage() {
@@ -178,5 +181,9 @@ public class TestDataFactory {
 
 	public IssueTemplate findIssueTemplateById(Long id) {
 		return issueTemplateRepository.findById(id).orElseThrow();
+	}
+
+	public List<ScrumTemplate> findScrumTemplatesByProjectId(Long projectId) {
+		return scrumTemplateRepository.findByProjectId(projectId);
 	}
 }
