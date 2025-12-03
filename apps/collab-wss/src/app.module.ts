90ad0c779d91fcf319cf7486aba291a68ed569df
Heesung Choi <gladhee@icloud.com>
2025-12-03 14:57:04 +0900

    feat(app): remove ClientModule from imports in AppModule

--- apps/collab-wss/src/app.module.ts
+++ apps/collab-wss/src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './modules/health/health.module';
-import { ClientModule } from './modules/client/client.module';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [HealthModule, ClientModule, AuthModule],
+  imports: [HealthModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
