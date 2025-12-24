3c4c7f6defa0d8684922717d32e1c7492db9f47b
seungsje <seungsje@student.42seoul.kr>
2025-12-24 16:45:41 +0900

    feat(SecurityConfig): add intenal api role and add filter order

--- apps/backend/src/main/java/scrumpledpaper/agiler/auth/filter/ApiKeyAuthFilter.java
+++ apps/backend/src/main/java/scrumpledpaper/agiler/auth/filter/ApiKeyAuthFilter.java
@@ -1,9 +1,14 @@
 package scrumpledpaper.agiler.auth.filter;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -14,16 +19,25 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class ApiKeyAuthFilter extends OncePerRequestFilter {
+
 	@Value("${app.api.key}")
 	private String expectedKey;
+	private static final String INTERNAL_API_ROLE = "ROLE_INTERNAL";
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 		throws ServletException, IOException {
+
 		String path = request.getRequestURI();
 		if (path.startsWith("/internal/")) {
 			String key = request.getHeader("X-API-KEY");
 			if (expectedKey.equals(key)) {
+				Authentication authentication = new PreAuthenticatedAuthenticationToken(
+					key,
+					null,
+					List.of(new SimpleGrantedAuthority(INTERNAL_API_ROLE))
+				);
+				SecurityContextHolder.getContext().setAuthentication(authentication);
 				filterChain.doFilter(request, response);
 			} else {
 				response.setStatus(HttpStatus.UNAUTHORIZED.value());
